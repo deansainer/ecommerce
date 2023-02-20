978e550de0dfe7e3ad23abd08485c9ec0d6e282d
deansainer <determind2k@gmail.com>
2023-02-20 15:48:20 +0100

    commit

--- apple_store/store_app/urls.py
+++ apple_store/store_app/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('cart/', views.cart, name='cart'),
     path('checkout/', views.checkout, name='checkout'),
     path('thanks_page/', views.thanks_page, name='thanks_page'),
-
+    path('update_item/', views.updateItem, name='update_item'),
 ]
--- apple_store/store_app/views.py
+++ apple_store/store_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import *
+from django.http import JsonResponse
+import json
 
 
 def store(request):
@@ -31,8 +33,30 @@ def checkout(request):
     return render(request, 'store_app/checkout.html', context)
 
 
-
 def thanks_page(request):
     return render(request, 'store_app/thanks_page.html')
 
 
+def updateItem(request):
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+    print('Action:', action)
+    print('Product:', productId)
+
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+    orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+    if action == 'add':
+        orderItem.quantity = (orderItem.quantity + 1)
+    elif action == 'remove':
+        orderItem.quantity = (orderItem.quantity - 1)
+
+    orderItem.save()
+    if orderItem.quantity <=0:
+        orderItem.delete()
+
+    return JsonResponse('Item was added', safe=False)
